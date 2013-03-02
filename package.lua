4febbf061327181b0077db290019fc550b59831a
Tomaz Muraus <tomaz@tomaz.me>
2013-03-01 20:49:13 -0800

    Bump version.

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "line-emitter",
-  version = "0.1.0",
+  version = "0.2.0",
   description = "Library for buffering chunks of data and emitting lines",
   author = "Tomaz Muraus <tomaz@tomaz.me>",
   contributors = {},
